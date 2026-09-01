776e7de4ff7395886fe4a3d1acdc3e7ad2622b02
Hicanos <ruda32168@gmail.com>
2026-09-01 12:48:07 +0900

    [Update] Test3&4
    CodeKata 3, 4 Upload

--- Week1/Java/src/main/java/org/example/Main.java
+++ Week1/Java/src/main/java/org/example/Main.java
@@ -7,11 +7,15 @@ public class Main
     public static void main(String[] args)
     {
         solution(5000,1024);
-        solution2(153,54);
+        solution2(13,54);
+        solution3(9,5);
+        BirthYear(7);
+
     }
 
     public static int solution(int num1, int num2)
     {
+        //두 수의 차
         if(num1<-50000||num1>50000||num2<-50000||num2>50000)
         {
             System.out.println("-50000~50000까지의 정수를 입력해주세요");
@@ -27,9 +31,10 @@ public class Main
 
     public static int solution2(int num1, int num2)
     {
-        if(num1<-50000||num1>50000||num2<-50000||num2>50000)
+        //두 수의 곱
+        if(num1<0||num1>100||num2<0||num2>100)
         {
-            System.out.println("-50000~50000까지의 정수를 입력해주세요");
+            System.out.println("0~100까지의 정수를 입력해주세요");
             return 0;
         }
         else
@@ -38,5 +43,37 @@ public class Main
             System.out.println(answer);
             return answer;
         }
+    }
+
+    public static int solution3(int num1, int num2)
+    {
+        //몫 구하기
+        if(num1<=0||num1>100||num2<=0||num2>100)
+        {
+            System.out.println("0~100까지의 정수를 입력해주세요");
+            return 0;
+        }
+        else
+        {
+            int answer = num1/num2;
+            System.out.println(answer);
+            return answer;
+        }
+    }
+
+    public static int BirthYear(int age)
+    {
+        //태어난 연도 구하기(2022년 기준)
+        if(age<=0||age>120)
+        {
+            System.out.println("1~120까지의 나이를 작성");
+            return 0;
+        }
+
+        int currentYear=2022;
+        int birthYear=currentYear-age+1;
+        int answer = birthYear;
+        return answer;
+
     }
 }
