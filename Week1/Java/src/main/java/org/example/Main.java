9dddab316fa377ef2d60c6b861c1f4f626fe98b7
Hicanos <ruda32168@gmail.com>
2026-09-01 11:24:30 +0900

    [Add]Test1
    CodeKata1

--- Week1/Java/src/main/java/org/example/Main.java
+++ Week1/Java/src/main/java/org/example/Main.java
@@ -4,15 +4,30 @@ package org.example;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main
 {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args)
+    {
+        solution(4,2);
+        solution(-3,-2);
+        solution(5000,-5000);
+        solution(50001,6);
+        solution(-57000,9);
+        solution(5,50020);
+        solution(10,-50001);
+        solution(50102,-59102);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+    public static int solution(int num1, int num2)
+    {
+        if(num1<-50000||num1>50000||num2<-50000||num2>50000)
+        {
+            System.out.println("-50000~50000까지의 정수를 입력해주세요");
+            return 0;
+        }
+        else
+        {
+            int answer = num1-num2;
+            System.out.println(answer);
+            return answer;
         }
     }
 }
